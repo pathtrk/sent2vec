ec6567084fd1ffb7987d633f797fec519a29ae26
Ryosuke Goto <ryosuke.goto@gmail.com>
2018-12-30 21:31:36 +0900

    fix the scropt for preprocessing

--- prepare_dataset_json.sh
+++ prepare_dataset_json.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /storage
-wget https://dumps.wikimedia.org/jawiki/latest/jawiki-latest-pages-articles-multistream.xml.bz2
-bzcat jawiki-latest-pages-articles-multistream.xml.bz2 | WikiExtractor -o /storage/wiki_articles --json --no-templates --processes 24 -
+# wget https://dumps.wikimedia.org/jawiki/latest/jawiki-latest-pages-articles-multistream.xml.bz2
+bzcat jawiki-latest-pages-articles-multistream.xml.bz2.1 | WikiExtractor.py -o wiki_articles --json --no-templates --processes 24 -
