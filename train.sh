753a6a692e72ce90efb0b83ef5df9f59eea9bdd2
Ryosuke Goto <ryosuke.goto@gmail.com>
2019-01-16 11:49:24 +0900

    add train.sh

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-./fasttext sent2vec -input /storage/sent2vec-ja-wiki.txt -output /storage/sent2vec-ja-wiki-model -minCount 8 -dim 700 -epoch 9 -lr 0.2 -wordNgrams 2 -loss ns -neg 10 -thread 24 -t 0.000005 -dropoutK 4 -minCountLabel 20 -bucket 4000000
+make
+fasttext sent2vec -input /storage/sent2vec-ja-wiki.txt -output /storage/model -minCount 8 -dim 700 -epoch 30 -lr 0.02 -wordNgrams 5 -loss ns -neg 10 -thread 24 -t 0.000005 -dropoutK 4 -minCountLabel 20 -bucket 4000000
